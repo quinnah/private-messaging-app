9200ffff3dcb9f3d8ac8a4710c34f483c636f924
Quinna Halim <quinnah@umich.edu>
2022-05-31 16:52:49 -0400

    Add session ID (user authentication) and user ID (identifier for exchanging messages)

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 const URL = "http://localhost:3000";
-const socket = io(URL, { autoConnect: false }); // Initialize client
+const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
-}); // catch-all listener, triggered for any events
+});
 
 export default socket;
